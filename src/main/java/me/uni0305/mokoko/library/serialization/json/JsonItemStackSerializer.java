874c388e65ff92d1bc50cecd4559230e70c7ad8a
Uni0305 <git@uni0305.me>
2024-12-03 18:12:39 +0900

    Refactor(serialization): Update itemstack serializer to use NBT API

--- src/main/java/me/uni0305/mokoko/library/serialization/json/JsonItemStackSerializer.java
+++ src/main/java/me/uni0305/mokoko/library/serialization/json/JsonItemStackSerializer.java
@@ -1,38 +1,33 @@
 package me.uni0305.mokoko.library.serialization.json;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
-import com.mojang.serialization.Dynamic;
-import com.mojang.serialization.JsonOps;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtOps;
-import net.minecraft.nbt.Tag;
-import org.bukkit.craftbukkit.v1_20_R3.inventory.CraftItemStack;
+import de.tr7zw.nbtapi.NBT;
+import de.tr7zw.nbtapi.iface.ReadWriteNBT;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class JsonItemStackSerializer implements JsonSerializer<ItemStack> {
+    private final Gson gson = new Gson();
 
     @Override
-    public @NotNull JsonElement serialize(@NotNull ItemStack obj) throws RuntimeException {
+    public @NotNull JsonElement serialize(@NotNull ItemStack obj) {
         try {
-            net.minecraft.world.item.ItemStack nms = CraftItemStack.asNMSCopy(obj);
-            CompoundTag nbt = nms.save(new CompoundTag());
-            Dynamic<Tag> tag = new Dynamic<>(NbtOps.INSTANCE, nbt);
-            Dynamic<JsonElement> json = tag.convert(JsonOps.INSTANCE);
-            return json.getValue();
+            ReadWriteNBT nbt = NBT.itemStackToNBT(obj);
+            String json = nbt.toString();
+            return gson.toJsonTree(json);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
-    public @NotNull ItemStack deserialize(@NotNull JsonElement src) throws RuntimeException {
+    public @Nullable ItemStack deserialize(@NotNull JsonElement src) throws RuntimeException {
         try {
-            Dynamic<JsonElement> json = new Dynamic<>(JsonOps.INSTANCE, src);
-            Dynamic<Tag> tag = json.convert(NbtOps.INSTANCE);
-            CompoundTag nbt = (CompoundTag) tag.getValue();
-            net.minecraft.world.item.ItemStack nms = net.minecraft.world.item.ItemStack.of(nbt);
-            return CraftItemStack.asBukkitCopy(nms);
+            String json = src.toString();
+            ReadWriteNBT nbt = NBT.parseNBT(json);
+            return NBT.itemStackFromNBT(nbt);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
